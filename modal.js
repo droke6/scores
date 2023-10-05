aef5ac6e34089352f26d8fc4a4d3630df3d22fc9
Daniel Roque <droque6@icloud.com>
2023-10-04 22:14:48 -0500

    Add seperate modal for each blue court. need to fix submit button for each blue court except blue 1

--- modal.js
+++ modal.js
@@ -1,17 +1,47 @@
-let modal = document.getElementById('myModal')
+let modalblue1 = document.getElementById('myModal')
+let modalblue2 = document.getElementById('blue2Modal')
+let modalblue3 = document.getElementById('blue3Modal')
+let modalblue4 = document.getElementById('blue4Modal')
+let modalblue17pm = document.getElementById('blue1Modal7')
+let modalblue27pm = document.getElementById('blue2Modal7')
+let modalblue37pm = document.getElementById('blue3Modal7')
+let modalblue47pm = document.getElementById('blue4Modal7')
 let courtChoice = document.getElementById('court-choice')
-
-courtChoice.onclick = function showModal() {
-    if (courtChoice.value == 'zero') {
-        document.querySelector('.modal').modal.style.display = 'none';
-    } else {
-        document.querySelector('.modal').modal.style.display = 'block';
-    }
-}
+let gameDay = document.getElementById('day-choice')
+let fridayTime = document.getElementById('friday-time')
 
 courtChoice.addEventListener('change', function showModal() {
-    document.querySelector('.modal').style.display = 'block'
+    if (courtChoice.value == 'zero'){
+        modalblue1.style.display = 'none';
+    } 
+    else if (courtChoice.value == 'blue1' && gameDay.value == 'friday' && fridayTime.value == '6pm') {
+        modalblue1.style.display = 'block';
+    } 
+    else if (courtChoice.value == 'blue2' && gameDay.value == 'friday' && fridayTime.value == '6pm'){
+        modalblue2.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue3' && gameDay.value == 'friday' && fridayTime.value == '6pm'){
+        modalblue3.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue4' && gameDay.value == 'friday' && fridayTime.value == '6pm'){
+        modalblue4.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue1' && gameDay.value == 'friday' && fridayTime.value == '7pm'){
+        modalblue17pm.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue2' && gameDay.value == 'friday' && fridayTime.value == '7pm'){
+        modalblue27pm.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue3' && gameDay.value == 'friday' && fridayTime.value == '7pm'){
+        modalblue37pm.style.display = 'block';
+    }
+    else if (courtChoice.value == 'blue4' && gameDay.value == 'friday' && fridayTime.value == '7pm'){
+        modalblue47pm.style.display = 'block';
+    }
+    else {
+    }
 })
+
 
 //////////////////////
 
@@ -28,13 +58,13 @@ const form = document.getElementById('myForm');
 function submitSuccessful() {
     successModal.style.display = 'block';
     myModal.style.display = 'none';
-    errorModal.style.display = 'none';
+    errorModal.style.display = 'none'
 }
 
 function submitError() {
     errorModal.style.display = 'block';
     successModal.style.display = 'none';
-    modal.style.display = 'block'
+    myModal.style.display = 'block'
 }
 
 function hideSubmitError() {
@@ -47,7 +77,7 @@ function hideSubmitSuccessful() {
 }
 
 function redirectToPage() {
-    window.location.href = 'https://droke6.github.io/scores/';
+    window.location.href = 'http://127.0.0.1:3000/index.html';
 }
 
 form.addEventListener('submit', function (e) {
